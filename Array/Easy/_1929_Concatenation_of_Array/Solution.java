e6851a0c28d764a5591177e4d7794bb2190527dd
Alexandru Nastase <nastase.alexandru100@gmail.com>
2022-06-10 19:36:38 +0100

    1929

---
+++ Array/Easy/_1929_Concatenation_of_Array/Solution.java
@@ -0,0 +1,15 @@
+package Array.Easy._1929_Concatenation_of_Array;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[2*len];
+
+        for(int i = 0; i < len; i++){
+            ans[i] = nums[i];
+            ans[i + len] = nums[i];
+        }
+
+        return ans;
+    }
+}
